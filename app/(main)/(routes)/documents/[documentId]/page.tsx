c2512cfe9432165d10dd766e23e2d6e843a63188
sushanth645 <sushanthlakkoju66@gmail.com>
2025-01-14 02:01:51 +0530

    final commit

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -45,12 +45,12 @@ const DocumentIdPage = () => {
             Not found
         </p>
     };
-
+    console.log("Document data:", document);
     return ( 
             <div className="pb-40">
                 <Cover preview url={document.coverImage}/>
               <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
-                  <Toolbar preview initialData={document}/>
+                  <Toolbar initialData={document}/>
                   <Editor
                   onChange={onChange}
                   initialContent={document.content}
